4e70000908fd2d42e26e66cead6609e6483af9e3
Lolamuss <lola.musslin@gmail.com>
2021-11-25 09:35:59 +0100

    create database

--- produits.sql
+++ produits.sql
@@ -13,3 +13,35 @@ INSERT INTO produits (numProduit, catégorie, nom, marque, prix) VALUES
 (4, 'tablette', 'Oxygen', 'Archos', 169),
 (5, 'tablette', 'iPad Air 2', 'Apple', 199),
 (6, 'écouteurs', 'airPods 2', 'Apple', 179);
+
+CREATE TABLE IF NOT EXISTS  clients (
+ email varchar(30) NOT NULL PRIMARY KEY,
+ motdepasse varchar(15) NOT NULL,
+ nom varchar(15),
+ prenom varchar(15),
+ ville varchar(15),
+ adresse varchar(20),
+ telephone int(10));
+
+INSERT INTO clients (email, motdepasse, nom, prenom, ville, adresse, telephone) VALUES
+('test', 'passe', 'lola', 'musslin', 'nice', 'ch.desoliviers', 0600000000);
+
+CREATE TABLE IF NOT EXISTS commandes (
+ idCommande int(5) NOT NULL PRIMARY KEY,
+ calendrier date,
+ email varchar(30), FOREIGN KEY (email) REFERENCES clients(email) ON DELETE CASCADE);
+
+INSERT INTO commandes (idCommande, calendrier, email) VALUES
+(1, 20200101, 'test');
+
+CREATE TABLE IF NOT EXISTS lignescommandes (
+  idlignecommandes int(5) NOT NULL PRIMARY KEY,
+  idCommande int(5), FOREIGN KEY(idCommande) REFERENCES commandes(idCommande) ON DELETE CASCADE,
+  idProduit INT, FOREIGN KEY(idProduit) REFERENCES produits(numProduit) ON DELETE CASCADE,
+  quantité int(3),
+  montant float(5.2));
+
+  INSERT INTO lignescommandes (idlignecommandes, idCommande, idProduit, quantité, montant) VALUES
+   (1,1,2,2,999.98);
+
+  
